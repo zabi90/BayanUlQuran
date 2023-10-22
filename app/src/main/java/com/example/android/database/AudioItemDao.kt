9f882aca52d6bf3a81ea403051618abc66db67e2
Zohaib Akram <zohaib.bin.akram@gmail.com>
2023-10-22 15:10:54 +0500

    upgrade the app to all new version of gradle and lib dependencies

--- app/src/main/java/com/example/android/database/AudioItemDao.kt
+++ app/src/main/java/com/example/android/database/AudioItemDao.kt
@@ -3,7 +3,7 @@ package com.example.android.database
 import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
-import androidx.room.OnConflictStrategy.REPLACE
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.example.android.models.AudioItem
 import kotlinx.coroutines.flow.Flow
@@ -14,7 +14,7 @@ interface AudioItemDao {
     @Query("SELECT * FROM AudioItem")
     fun getAll(): Flow<List<AudioItem>>
 
-    @Insert(onConflict = REPLACE)
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insert(audioItem: AudioItem)
 
     @Delete
